548bd35f9a8fbdc2b96b5954bf36176e499344cc
Samuel Cormier-Iijima <samuel@cormier-iijima.com>
2021-12-13 21:43:33 -0500

    Day 12 + 13

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+#![feature(generators)]
+
 use clap::{App, Arg};
 
 mod day1;
 mod day10;
 mod day11;
+mod day12;
+mod day13;
 mod day2;
 mod day3;
 mod day4;
@@ -43,6 +47,8 @@ fn main() {
         9 => day9::solve(),
         10 => day10::solve(),
         11 => day11::solve(),
+        12 => day12::solve(),
+        13 => day13::solve(),
         _ => (),
     }
 }
